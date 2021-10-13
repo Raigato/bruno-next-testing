4e1808ad0e637263343493dddfbeef2453f0e928
Raigato <gabriel.raymondou@gmail.com>
2021-10-13 18:34:41 +0200

    added too small feature

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export interface ICounterProps {
   description: string;
@@ -8,38 +8,59 @@ export interface ICounterProps {
 const Counter = ({ description, defaultCount }: ICounterProps) => {
   const [count, setCount] = useState(defaultCount);
   const [step, setStep] = useState(1);
+  const [bigEnough, setBigEnough] = useState(defaultCount >= 15);
+
+  useEffect(() => {
+    let active = true;
+
+    if (count >= 15) {
+      setTimeout(() => {
+        if (active) {
+          setBigEnough(true);
+        }
+      }, 300);
+    }
+
+    return () => {
+      active = false;
+    };
+  }, [count]);
 
   return (
     <div>
       <h2>
         DESC: {description} - DC: {defaultCount}
       </h2>
-      <label htmlFor="step">
-        Step:{" "}
-        <input
-          type="number"
-          name="step"
-          id="step"
-          value={step}
-          onChange={(evt) => {
-            setStep(parseInt(evt.target.value) || 1);
-          }}
-        />
-      </label>
-      <br />
-      <button
-        aria-label="Decrement Counter"
-        onClick={() => setCount(count - step)}
-      >
-        -
-      </button>
-      Current Count: {count}
-      <button
-        aria-label="Increment Counter"
-        onClick={() => setTimeout(() => setCount(count + step), 200)}
-      >
-        +
-      </button>
+      <div>
+        <label htmlFor="step">
+          Step:{" "}
+          <input
+            type="number"
+            name="step"
+            id="step"
+            value={step}
+            onChange={(evt) => {
+              setStep(parseInt(evt.target.value) || 1);
+            }}
+          />
+        </label>
+      </div>
+      <div>
+        <button
+          aria-label="Decrement Counter"
+          onClick={() => setCount(count - step)}
+        >
+          -
+        </button>
+        Current Count: {count}
+        <button
+          aria-label="Increment Counter"
+          onClick={() => setTimeout(() => setCount(count + step), 200)}
+        >
+          +
+        </button>
+      </div>
+      {!bigEnough && <div>I am too small</div>}
     </div>
   );
 };
